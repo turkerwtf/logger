826050fbf3bd2f7dfc4dc0a4b105812260d81cca
Türker <63150613+turkerssh@users.noreply.github.com>
2025-01-10 15:41:01 +0300

    Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,51 +1,94 @@
 const chalk = require("chalk");
 
+
 const defaultVariable = chalk.gray.bold(
+
   `[${new Date().toLocaleTimeString()}]:`,
+
 );
 
-function info({ type, message }) {
+
+function info({ type = "info", message }) {
+
   console.log(
+
     `${defaultVariable} ${chalk.blue.bold(`[${type}]:`)} ${chalk.blue(
+
       message,
+
     )}`,
+
   );
+
 }
 
-function error({ type, message }) {
+
+function error({ type = "error", message }) {
+
   console.log(
+
     `${defaultVariable} ${chalk.red.bold(`[${type}]:`)} ${chalk.red(message)}`,
+
   );
+
 }
 
-function success({ type, message }) {
+
+function success({ type = "success", message }) {
+
   console.log(
+
     `${defaultVariable} ${chalk.green.bold(`[${type}]:`)} ${chalk.green(
+
       message,
+
     )}`,
+
   );
+
 }
 
-function warn({ type, message }) {
+
+function warn({ type = "warn", message }) {
+
   console.log(
+
     `${defaultVariable} ${chalk.yellow.bold(`[${type}]:`)} ${chalk.yellow(
+
       message,
+
     )}`,
+
   );
+
 }
 
-function debug({ type, message }) {
+
+function debug({ type = "debug", message }) {
+
   console.log(
+
     `${defaultVariable} ${chalk.magenta.bold(`[${type}]:`)} ${chalk.magenta(
+
       message,
+
     )}`,
+
   );
+
 }
+
 
 module.exports = {
+
   info,
+
   error,
+
   success,
+
   warn,
+
   debug,
+
 };
